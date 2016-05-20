c5d76d420c69343360743fc912b9c6355c5c5722
Kratos <sergiy.katry4@gmail.com>
2016-05-20 12:28:35 +0300

    First init

---
+++ Assets/Script/RoadTilesGeneratorScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadTilesGeneratorScript : MonoBehaviour
+{
+
+	[SerializeField] private GameObject roadTilePref;
+	private Vector3 tileSize;
+
+	// Use this for initialization
+	void Start()
+	{
+		float tileSizeX = roadTilePref.GetComponent<BoxCollider>().size.x;
+		tileSize = new Vector3(tileSizeX, 0, 0);
+
+		SimplePool.Preload(roadTilePref, 5);
+		for (int i = 0; i < 5; i++)
+		{
+			SpawnTile();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		SpawnTile();
+		DespawnTile();
+	}
+
+	void SpawnTile()
+	{
+		
+		Vector3 prevTilePos;
+		if (transform.childCount > 0)
+			prevTilePos = transform.GetChild(transform.childCount - 1).localPosition;
+		else
+			prevTilePos = Vector3.zero - tileSize;
+		
+		Vector3 nextPos = prevTilePos + tileSize;
+		GameObject tile = SimplePool.Spawn(roadTilePref, nextPos, roadTilePref.transform.rotation);
+		tile.transform.parent = transform;
+	}
+
+	void DespawnTile()
+	{
+		GameObject tile = transform.GetChild(0).gameObject;
+		tile.transform.parent = null;
+		SimplePool.Despawn(tile);
+	}
+}
