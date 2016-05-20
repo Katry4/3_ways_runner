6798024e08b36fdf92cf1cb624ae8ad094d40ead
Kratos <sergiy.katry4@gmail.com>
2016-05-20 15:04:12 +0300

    Moving forward capsule and object pooled roads

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public static float VisibleDistance = 5;
+
+	private float _speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * _speed * Time.deltaTime;
+	}
+}
--- Assets/Script/RoadTilesGeneratorScript.cs
+++ Assets/Script/RoadTilesGeneratorScript.cs
@@ -6,45 +6,79 @@ public class RoadTilesGeneratorScript : MonoBehaviour
 
 	[SerializeField] private GameObject roadTilePref;
 	private Vector3 tileSize;
+	private Transform lastTile;
+	[SerializeField] private PlayerScript _player;
+
+	bool _isInited = false;
 
 	// Use this for initialization
-	void Start()
+	void OnEnable()
 	{
+		ClearChildren();
+
 		float tileSizeX = roadTilePref.GetComponent<BoxCollider>().size.x;
 		tileSize = new Vector3(tileSizeX, 0, 0);
+		lastTile = null;
+
+
+		if (_player == null)
+		{
+			_player = FindObjectOfType<PlayerScript>();
+		}
 
 		SimplePool.Preload(roadTilePref, 5);
-		for (int i = 0; i < 5; i++)
-		{
-			SpawnTile();
-		}
 	}
 	
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
-		SpawnTile();
-		DespawnTile();
+		if (_isInited)
+		{
+			CheckDistanceToGenerateTile();
+		}
+	}
+
+	private void CheckDistanceToGenerateTile()
+	{
+		
+		if (lastTile == null || _player.transform.position.x + PlayerScript.VisibleDistance > lastTile.position.x)
+		{
+			SpawnTile();
+		}
+		Transform firstTile = transform.GetChild(0);
+		if (_player.transform.position.x - 1 > firstTile.position.x)
+		{
+			DespawnTile(firstTile);
+		}
 	}
 
 	void SpawnTile()
 	{
-		
 		Vector3 prevTilePos;
 		if (transform.childCount > 0)
-			prevTilePos = transform.GetChild(transform.childCount - 1).localPosition;
+			prevTilePos = transform.GetChild(transform.childCount - 1).position;
 		else
-			prevTilePos = Vector3.zero - tileSize;
-		
+			prevTilePos = transform.position - tileSize;
+
 		Vector3 nextPos = prevTilePos + tileSize;
 		GameObject tile = SimplePool.Spawn(roadTilePref, nextPos, roadTilePref.transform.rotation);
-		tile.transform.parent = transform;
+		lastTile = tile.transform;
+		lastTile.parent = transform;
 	}
 
-	void DespawnTile()
+	void DespawnTile(Transform firstTile)
 	{
-		GameObject tile = transform.GetChild(0).gameObject;
-		tile.transform.parent = null;
-		SimplePool.Despawn(tile);
+		firstTile.parent = null;
+		SimplePool.Despawn(firstTile.gameObject);
+	}
+
+	private void ClearChildren()
+	{
+		foreach(Transform child in transform)
+		{
+			Destroy(child);
+		}
+
+		_isInited = true;
 	}
 }
